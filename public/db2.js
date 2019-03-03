c5baa003799ce02bf77a2adcdd650dadba46004a
sade <Ss>
2019-03-03 10:47:42 +0000

    first commit

---
+++ public/db2.js
@@ -0,0 +1,123 @@
+ var chartData2 = [ 
+                                         {
+                                         "block": "1H",
+                                         "count": 53
+                                         },
+                                         
+                                         {
+                                         "block": "2H",
+                                         "count": 59
+                                         },
+                                         
+                                         {
+                                         "block": "3H",
+                                         "count": 61
+                                         },
+                                         
+                                         {
+                                         "block": "4H",
+                                         "count": 53
+                                         },
+                                         
+                                         {
+                                         "block": "5H",
+                                         "count": 66
+                                         },
+                                         
+                                         {
+                                         "block": "6H",
+                                         "count": 66
+                                         },
+                                         
+                                         {
+                                         "block": "7H",
+                                         "count": 61
+                                         },
+                                         
+                                         {
+                                         "block": "8H",
+                                         "count": 58
+                                         },
+                                         
+                                         {
+                                         "block": "9H",
+                                         "count": 49
+                                         },
+                                         
+                                         {
+                                         "block": "10H",
+                                         "count": 74
+                                         },
+                                         
+                                         {
+                                         "block": "11H",
+                                         "count": 59
+                                         },
+                                         
+                                         {
+                                         "block": "12H",
+                                         "count": 74
+                                         },
+                                         
+                                         {
+                                         "block": "13H",
+                                         "count": 45
+                                         },
+                                         
+                                         {
+                                         "block": "14H",
+                                         "count": 57
+                                         },
+                                         
+                                         {
+                                         "block": "15H",
+                                         "count": 62
+                                         },
+                                         
+                                         {
+                                         "block": "16H",
+                                         "count": 64
+                                         },
+                                         
+                                         {
+                                         "block": "17H",
+                                         "count": 62
+                                         },
+                                         
+                                         {
+                                         "block": "18H",
+                                         "count": 56
+                                         },
+                                         
+                                         {
+                                         "block": "19H",
+                                         "count": 63
+                                         },
+                                         
+                                         {
+                                         "block": "20H",
+                                         "count": 52
+                                         },
+                                         
+                                         {
+                                         "block": "21H",
+                                         "count": 65
+                                         },
+                                         
+                                         {
+                                         "block": "22H",
+                                         "count": 60
+                                         },
+                                         
+                                         {
+                                         "block": "23H",
+                                         "count": 44
+                                         },
+                                         
+                                         {
+                                         "block": "24H",
+                                         "count": 45
+                                         },
+                                         
+                                ];
+                          
